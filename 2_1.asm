0bc8e9fa22d4e27d643975643aabff694b4cd7b5
JacobMarquiss <Jacob.Marquiss@warwick.ac.uk>
2018-11-01 14:01:48 +0000

    Make changes so whole grid is generated, also change the pattern made

--- 2_1.asm
+++ 2_1.asm
@@ -1,17 +1,20 @@
 | declare two constants
-  i: dc.b #16
+  i: dc.w #65535
   sum: dc.b #0
-  q: dc.b #20
+  q: dc.b #2
 | declare one byte of storage in main memory for the result
   result: ds.b 1
 | declare array
   array: ds.b 128
   move.w #array, A0
-  move.b i, D0
-  move.b sum, D1
-  loop: add q, D1
+  move.w i, D0
+  move.w sum, D1
+  move.w q, D2
+  loop: add D2, D1
 | move the value of sum to main memory
   move.b D1, (A0)
+  ror.w D2
+  sub.w #128, D2
   inc A0
   dec D0
   bpl loop
